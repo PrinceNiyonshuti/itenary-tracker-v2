2665a8e6dd874fc6f9470e8592cc03c2150c16c9
princeniyonshuti <npprince47@gmail.com>
2021-10-30 20:34:05 +0200

    done but delete not working well

---
+++ src/Layout/Main.js
@@ -0,0 +1,54 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import Itenaries from "../Components/Itenaries";
+
+const Main = () => {
+	const [itenaryData, setItenaryData] = useState(null);
+
+	useEffect(() => {
+		fetch(`http://localhost:8000/itenaryList`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setItenaryData(data);
+			});
+	}, []);
+
+	// Remove Itenary
+	const deleteItenary = (ItenaryId) => {
+		const filteredItenaries = itenaryData.filter(
+			(itenary) => itenary.id !== ItenaryId
+		);
+		setItenaryData(filteredItenaries);
+
+		// const filteredItenaries = fetch(
+		// 	`http://localhost:8000/itenaryList/` + ItenaryId,
+		// 	{
+		// 		method: "DELETE",
+		// 	}
+		// ).then(() => {
+		// 	console.log("Itenary Deleted");
+		// 	history.push("/");
+		// });
+
+		// setItenaryData(...itenaryData,filteredItenaries);
+	};
+	return (
+		<div>
+			<div className="w-full p-4">
+				<ul>
+					{itenaryData && (
+						<Itenaries
+							itenaryData={itenaryData}
+							deleteItenary={deleteItenary}
+						/>
+					)}
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Main;
